eca9dd9de07d14377dc22e46690b7400811a3364
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2020-12-24 05:19:13 -0800

    Fix up windows stuff.

--- Common_3/TheForge.cmake
+++ Common_3/TheForge.cmake
@@ -15,17 +15,17 @@ source_group(OS\\Camera FILES ${OS_CAMERA_FILES})
 source_group(OS\\Math FILES ${OS_MATH_FILES})
 
 set(OS_IMAGE_FILES
-    ../The-Forge/Common_3/ThirdParty/OpenSource/basis_universal/transcoder/basisu_transcoder.cpp
-)
+        ../The-Forge/Common_3/ThirdParty/OpenSource/basis_universal/transcoder/basisu_transcoder.cpp
+        )
 
 source_group(OS\\Image FILES ${OS_IMAGE_FILES})
 
 set(OS_MACOS_FILES
-    ../The-Forge/Common_3/OS/macOS/macOSBase.mm
-    ../The-Forge/Common_3/OS/macOS/macOSFileSystem.mm
-    ../The-Forge/Common_3/OS/macOS/macOSLog.cpp
-    ../The-Forge/Common_3/OS/macOS/macOSThread.cpp
-)
+        ../The-Forge/Common_3/OS/macOS/macOSBase.mm
+        ../The-Forge/Common_3/OS/macOS/macOSFileSystem.mm
+        ../The-Forge/Common_3/OS/macOS/macOSLog.cpp
+        ../The-Forge/Common_3/OS/macOS/macOSThread.cpp
+        )
 
 if (APPLE_PLATFORM MATCHES ON)
     source_group(OS\\macOS FILES ${OS_MACOS_FILES})
@@ -33,36 +33,36 @@ if (APPLE_PLATFORM MATCHES ON)
     set(OS_PLATFORM_SPECIFIC_FILES ${OS_MACOS_FILES})
 
     set(RENDERER_FILES ${RENDERER_FILES}
-        ../The-Forge/Common_3/Renderer/Metal/MetalRaytracing.mm
-        ../The-Forge/Common_3/Renderer/Metal/MetalMemoryAllocator.h
-        ../The-Forge/Common_3/Renderer/Metal/MetalRenderer.mm
-        ../The-Forge/Common_3/Renderer/Metal/MetalShaderReflection.mm
-    )
+            ../The-Forge/Common_3/Renderer/Metal/MetalRaytracing.mm
+            ../The-Forge/Common_3/Renderer/Metal/MetalMemoryAllocator.h
+            ../The-Forge/Common_3/Renderer/Metal/MetalRenderer.mm
+            ../The-Forge/Common_3/Renderer/Metal/MetalShaderReflection.mm
+            )
 
     set(CMAKE_CXX_FLAGS "-x objective-c++")
 endif()
 
 set(OS_WINDOWS_FILES
-    ../The-Forge/Common_3/OS/Windows/WindowsBase.cpp
-    ../The-Forge/Common_3/OS/Windows/WindowsFileSystem.cpp
-    ../The-Forge/Common_3/OS/Windows/WindowsLog.cpp
-    ../The-Forge/Common_3/OS/Windows/WindowsStackTraceDump.cpp
-    ../The-Forge/Common_3/OS/Windows/WindowsStackTraceDump.h
-    ../The-Forge/Common_3/OS/Windows/WindowsThread.cpp
-    ../The-Forge/Common_3/OS/Windows/WindowsTime.cpp
-)
+        ../The-Forge/Common_3/OS/Windows/WindowsBase.cpp
+        ../The-Forge/Common_3/OS/Windows/WindowsFileSystem.cpp
+        ../The-Forge/Common_3/OS/Windows/WindowsLog.cpp
+        ../The-Forge/Common_3/OS/Windows/WindowsStackTraceDump.cpp
+        ../The-Forge/Common_3/OS/Windows/WindowsStackTraceDump.h
+        ../The-Forge/Common_3/OS/Windows/WindowsThread.cpp
+        ../The-Forge/Common_3/OS/Windows/WindowsTime.cpp
+        )
 
-if (WINDOWS_PLATFORM MATCHES ON)
+if (WINDOWS MATCHES ON)
     source_group(OS\\Windows FILES ${OS_WINDOWS_FILES})
 
     set(OS_PLATFORM_SPECIFIC_FILES ${OS_WINDOWS_FILES})
 
     set(RENDERER_FILES ${RENDERER_FILES}
-        ../The-Forge/Common_3/Renderer/Vulkan/Vulkan.cpp
-        ../The-Forge/Common_3/Renderer/Vulkan/VulkanCapsBuilder.h
-        ../The-Forge/Common_3/Renderer/Vulkan/VulkanRaytracing.cpp
-        ../The-Forge/Common_3/Renderer/Vulkan/VulkanShaderReflection.cpp
-    )
+            ../The-Forge/Common_3/Renderer/Vulkan/Vulkan.cpp
+            ../The-Forge/Common_3/Renderer/Vulkan/VulkanCapsBuilder.h
+            ../The-Forge/Common_3/Renderer/Vulkan/VulkanRaytracing.cpp
+            ../The-Forge/Common_3/Renderer/Vulkan/VulkanShaderReflection.cpp
+            )
 endif()
 
 source_group(Renderer FILES ${RENDERER_FILES})
@@ -80,23 +80,23 @@ source_group(Middleware_3\\ECS FILES ${MIDDLEWARE_ECS_FILES})
 source_group(Middleware_3\\UI FILES ${MIDDLEWARE_UI_FILES})
 
 add_library(The-Forge STATIC
-    ${OS_INTERFACES_FILES}
-    ${OS_INPUT_FILES}
-    ${OS_FILESYSTEM_FILES}
-    ${OS_MEMORYMANAGER_FILES}
-    ${OS_CAMERA_FILES}
-    ${OS_MATH_FILES}
-    ${OS_IMAGE_FILES}
-    ${OS_PLATFORM_SPECIFIC_FILES}
-    ${RENDERER_FILES}
-    ${THIRDPARTY_OSS_EASTL_FILES}
-    ${THIRDPARTY_OSS_TINYEXR_FILES}
-    ${LOGGING_FILES}
-    ${OS_CORE_FILES}
-    ${MIDDLEWARE_ECS_FILES}
-    ${MIDDLEWARE_UI_FILES}
-    ${RMEM_FILES}
-)
+        ${OS_INTERFACES_FILES}
+        ${OS_INPUT_FILES}
+        ${OS_FILESYSTEM_FILES}
+        ${OS_MEMORYMANAGER_FILES}
+        ${OS_CAMERA_FILES}
+        ${OS_MATH_FILES}
+        ${OS_IMAGE_FILES}
+        ${OS_PLATFORM_SPECIFIC_FILES}
+        ${RENDERER_FILES}
+        ${THIRDPARTY_OSS_EASTL_FILES}
+        ${THIRDPARTY_OSS_TINYEXR_FILES}
+        ${LOGGING_FILES}
+        ${OS_CORE_FILES}
+        ${MIDDLEWARE_ECS_FILES}
+        ${MIDDLEWARE_UI_FILES}
+        ${RMEM_FILES}
+        )
 
 set_property(TARGET The-Forge PROPERTY CXX_STANDARD 17)
 
