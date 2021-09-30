302813f171508aa5989b849c768bf6eeb873cd4b
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2021-09-29 23:17:08 -0700

    Reorganize all of OS into the appropriate groups for 1.49

--- Common_3/TheForge.cmake
+++ Common_3/TheForge.cmake
@@ -8,7 +8,8 @@ source_group(OS\\Core FILES ${OS_CORE_FILES})
 
 source_group(OS\\FileSystem FILES ${OS_FILESYSTEM_FILES})
 
-source_group(OS\\Fonts FILES ${OS_FONT_FILES} ${OS_FONT_SHADER_FILES})
+source_group(OS\\Fonts FILES ${OS_FONT_FILES})
+source_group(OS\\Fonts\\Shaders FILES ${OS_FONT_SHADER_FILES})
 
 source_group(OS\\Input FILES ${OS_INPUT_FILES})
 
@@ -20,13 +21,15 @@ source_group(OS\\Math FILES ${OS_MATH_FILES})
 
 source_group(OS\\MemoryTracking FILES ${OS_MEMORYTRACKING_FILES})
 
-source_group(OS\\Middleware FILES ${OS_MIDDLEWARE_FILES} ${OS_MIDDLEWARE_PANINI_SHADER_FILES})
+source_group(OS\\Middleware FILES ${OS_MIDDLEWARE_FILES})
+source_group(OS\\Middleware\\Panini FILES ${OS_MIDDLEWARE_PANINI_SHADER_FILES})
 
 source_group(OS\\Profiler FILES ${OS_PROFILER_FILES})
 
 source_group(OS\\Scripting FILES ${OS_SCRIPTING_FILES})
 
-source_group(OS\\UI FILES ${OS_UI_FILES} ${OS_UI_SHADER_FILES})
+source_group(OS\\UI FILES ${OS_UI_FILES})
+source_group(OS\\UI\\Shaders FILES ${OS_UI_SHADER_FILES})
 
 set(OS_IMAGE_FILES
         ../The-Forge/Common_3/ThirdParty/OpenSource/basis_universal/transcoder/basisu_transcoder.cpp
@@ -83,32 +86,38 @@ source_group(Middleware_3\\ECS FILES ${MIDDLEWARE_ECS_FILES})
 
 source_group(Middleware_3\\UI FILES ${MIDDLEWARE_UI_FILES})
 
-add_library(The-Forge STATIC
+add_library(The-Forge-Dependencies STATIC
+        ${RMEM_FILES}
+        ${GAINPUT_STATIC_FILES}
+)
+
+add_library(OS STATIC
         ${OS_CAMERA_FILES}
         ${OS_CORE_FILES}
         ${OS_FILESYSTEM_FILES}
         ${OS_FONT_FILES}
+        ${OS_FONT_SHADER_FILES}
         ${OS_INPUT_FILES}
         ${OS_INTERFACES_FILES}
         ${OS_LOGGING_FILES}
         ${OS_MATH_FILES}
         ${OS_MEMORYTRACKING_FILES}
         ${OS_MIDDLEWARE_FILES}
+        ${OS_MIDDLEWARE_PANINI_SHADER_FILES}
         ${OS_PROFILER_FILES}
         ${OS_SCRIPTING_FILES}
         ${OS_UI_FILES}
+        ${OS_UI_SHADER_FILES}
         ${MIDDLEWARE_ECS_FILES}
         ${MIDDLEWARE_UI_FILES}
-        ${RMEM_FILES}
-        ${GAINPUT_STATIC_FILES}
-        )
+)
 
-set_property(TARGET The-Forge PROPERTY CXX_STANDARD 17)
+set_property(TARGET OS PROPERTY CXX_STANDARD 17)
 
-target_link_libraries(The-Forge ${RENDER_LIBRARIES} ${GLOBAL_LIBRARIES})
-target_include_directories(The-Forge PUBLIC ${RENDER_INCLUDES})
+target_link_libraries(OS ${RENDER_LIBRARIES} ${GLOBAL_LIBRARIES})
+target_include_directories(OS PUBLIC ${RENDER_INCLUDES})
 
-target_compile_definitions(The-Forge PUBLIC ${GLOBAL_DEFINES})
+target_compile_definitions(OS PUBLIC ${GLOBAL_DEFINES})
 
 if (APPLE_PLATFORM MATCHES ON)
     set_property (TARGET The-Forge APPEND_STRING PROPERTY COMPILE_FLAGS "-fobjc-arc")
