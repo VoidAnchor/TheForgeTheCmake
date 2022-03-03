38b5ed7433379f3cba3397640e2b42f5abd5463e
Jonathan "Geenz" Goodman <geenz@geenzo.com>
2022-03-03 00:16:57 -0800

    Add switch for macOS bundles
    Still not functional.  Need to figure out scripts and the like.

--- Examples_3/Unit_Tests/01_Transformations.cmake
+++ Examples_3/Unit_Tests/01_Transformations.cmake
@@ -4,7 +4,11 @@ set(TRANFROMATIONS_FILES
     ../../../The-Forge/Examples_3/Unit_Tests/src/01_Transformations/01_Transformations.cpp
 )
 
-add_executable(01_Transformations ${TRANFROMATIONS_FILES})
+if(${APPLE_PLATFORM} MATCHES ON)
+    add_executable(01_Transformations MACOSX_BUNDLE ${TRANFROMATIONS_FILES})
+else()
+    add_executable(01_Transformations ${TRANSFORMATIONS_FILES})
+endif()
 
 target_link_libraries(01_Transformations The-Forge)
 
